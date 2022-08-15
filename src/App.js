0ca96a2a5a7467df9a4d22b1b1af76a641035977
aswinc20y <aswin.aryal@c20y.com>
2022-08-15 11:11:32 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You again updated have all weight to be put on! and we have updated
+          the code as well
         </p>
         <a
           className="App-link"
